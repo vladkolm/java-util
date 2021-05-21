0ddbf8fd229def2f1a33a096f4196d053869fde4
vladkolm <vladimir.kolmanovitch@gmail.com>
2021-05-21 12:13:57 +0300

    Initial commit

---
+++ utils/src/main/java/info/vladkolm/utils/LazySingleton.java
@@ -0,0 +1,22 @@
+package info.vladkolm.utils;
+
+import java.util.function.Supplier;
+
+public class LazySingleton<T> {
+    private final Supplier<T> supplier;
+    private volatile T object;
+
+    public LazySingleton(Supplier<T> supplier) {
+        this.supplier = supplier;
+    }
+
+    public T createOrGet() {
+        if(object != null) return object;
+        return createOrGetWithLock();
+    }
+
+    private synchronized T createOrGetWithLock() {
+        if(object == null) object = supplier.get();
+        return object;
+    }
+}
