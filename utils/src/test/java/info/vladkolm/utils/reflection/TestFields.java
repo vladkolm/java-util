dfd3576070ddf6956765d47c730c57788152b65b
vlad <vladimir.kolmanovitch@gmail.com>
2021-10-10 14:15:27 +0300

    Refactoring nameOf methods

--- utils/src/test/java/info/vladkolm/utils/reflection/TestFields.java
+++ utils/src/test/java/info/vladkolm/utils/reflection/TestFields.java
@@ -4,18 +4,16 @@ import info.vladkolm.utils.reflection.classes.ClassWithMethods;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static info.vladkolm.utils.reflection.Fields.nameOf;
-
 public class TestFields {
     @Test
     public void testNameOfStaticField() {
-        String name = nameOf(() -> ClassWithMethods.msg);
+        String name = Fields.nameOf(() -> ClassWithMethods.msg);
         Assertions.assertEquals("msg", name);
     }
 
     @Test
     public void testNameOfField() {
-        String name = nameOf((ClassWithMethods cwm) -> cwm.message);
+        String name = Fields.nameOf((ClassWithMethods cwm) -> cwm.message);
         Assertions.assertEquals("message", name);
     }
 }
--- utils/src/test/java/info/vladkolm/utils/reflection/TestMethods.java
+++ utils/src/test/java/info/vladkolm/utils/reflection/TestMethods.java
@@ -1,33 +1,29 @@
 package info.vladkolm.utils.reflection;
 
-import info.vladkolm.utils.reflection.classes.ClassWithMethods;
+import info.vladkolm.utils.reflection.classes.ClassWithGenericMethod;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import info.vladkolm.utils.reflection.classes.ClassWithGenericMethod;
-
-import static info.vladkolm.utils.reflection.MethodInfo.*;
-import static info.vladkolm.utils.reflection.Methods.nameOf;
 
 public class TestMethods {
     @Test
     public void nameOf_usingMethodReference() {
-        String name = nameOf(ClassWithGenericMethod::genFun);
+        String name = Methods.nameOf(ClassWithGenericMethod::genFun);
         Assertions.assertEquals("genFun", name);
     }
     @Test
     public void nameOf_usingMethodReference2() {
-        String name = nameOf(ClassWithGenericMethod::genFun2);
+        String name = Methods.nameOf(ClassWithGenericMethod::genFun2);
         Assertions.assertEquals("genFun2", name);
     }
     @Test
     public void nameOf_usingLambda() {
-        String name = nameOf(() -> ClassWithGenericMethod.genFun(""));
+        String name = Methods.nameOf(() -> ClassWithGenericMethod.genFun(""));
         Assertions.assertEquals("genFun", name);
     }
 
     @Test
     public void testNameOfGenericMethodAsLambda() {
-        String name = nameOf(() -> ClassWithGenericMethod.genFun(""));
+        String name = Methods.nameOf(() -> ClassWithGenericMethod.genFun(""));
         Assertions.assertEquals("genFun", name);
     }
 }
