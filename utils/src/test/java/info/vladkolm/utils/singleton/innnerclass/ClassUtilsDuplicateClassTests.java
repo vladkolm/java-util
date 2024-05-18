afd2cf34fb59f955ff1febd1f53b804b104caad3
vlad <vladimir.kolmanovitch@gmail.com>
2024-05-18 09:49:49 +0300

    Add unit test for class duplication

--- utils/src/test/java/info/vladkolm/utils/singleton/innnerclass/ClassUtilsDuplicateClassTests.java
+++ utils/src/test/java/info/vladkolm/utils/singleton/innnerclass/ClassUtilsDuplicateClassTests.java
@@ -1,6 +1,7 @@
 package info.vladkolm.utils.singleton.innnerclass;
 
 import info.vladkolm.utils.reflection.ClassUtils;
+import info.vladkolm.utils.reflection.ClassUtils.ClassLoaderEx;
 import info.vladkolm.utils.singleton.innerclass.BillPughSupplier;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -11,5 +12,22 @@ public class ClassUtilsDuplicateClassTests {
         Class<?> clazz = ClassUtils.duplicateClass(BillPughSupplier.class);
         Assertions.assertNotNull(clazz);
         Assertions.assertNotEquals(BillPughSupplier.class, clazz);
+    }
+
+    // Asserts recursively, that all internal classes have the same ClassLoader
+    void assertRecursive(ClassLoader classLoader, Class<?> clazz){
+        Assertions.assertEquals(classLoader, clazz.getClassLoader());
+        Class<?>[] innerClasses = clazz.getDeclaredClasses();
+        for(Class<?> innerClass : innerClasses) {
+            assertRecursive(classLoader, innerClass);
+        }
+    }
+
+    @Test
+    public void duplicateClass_ClassLoaderTest() {
+        Class<?> clazz = ClassUtils.duplicateClass(BillPughSupplier.class);
+        ClassLoader classLoader = clazz.getClassLoader();
+        Assertions.assertSame(classLoader.getClass(), ClassLoaderEx.class);
+        assertRecursive(classLoader, clazz);
     }
 }
